7eff09a7ebb8c20739819302b2c9deb0c0269ac1
Matt Young <matt.young.1@outlook.com>
2024-03-17 17:26:31 +1000

    Move to spectrum for computing FFT - works well

--- scripts/process.py
+++ scripts/process.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq, rfft, rfftfreq, fftshift
 from matplotlib.animation import FuncAnimation
 import librosa
+from spectrum import Periodogram
+from spectrum import tools as stools
 
 # This script is used to process music data in the FLAC file format, compute the FFT, and serialise it to
 # Protocol Buffers.
@@ -21,7 +23,7 @@ import librosa
 # - https://www.youtube.com/watch?v=aQKX3mrDFoY
 
 # Number of samples that constitutes one spectrum block
-BLOCK_SIZE = 2048
+BLOCK_SIZE = 1024
 
 # Number of bars we want to draw
 NUM_BARS = 32
@@ -61,40 +63,35 @@ def main():
     blocks = np.split(mono, range(BLOCK_SIZE, len(mono), BLOCK_SIZE))
     print(f"num blocks: {len(blocks)} block shape: {blocks[0].shape}")
 
-    # compute FFT for each block - we use rfft which is the real valued fft
-    # for block in blocks:
-    #     yf = rfft(block)
-    #     xf = rfftfreq(len(block), 1 / sampling_rate)
-    #
-    #     plt.hist(np.abs(yf), bins=NUM_BARS, range=[FREQ_MIN, FREQ_MAX])
-    #     plt.show()
-
     fig, ax = plt.subplots(1, 1)
     fig.set_size_inches(5, 5)
 
     def animate(i):
         ax.clear()
         block = blocks[i]
-        yf = rfft(block) #**2 # this should be the power spectrum -> FIXME looks kinda weird??
-        xf = rfftfreq(len(block), 1 / sampling_rate)
-
-        # convert power spectrum to dB
-        # FIXME looks weird
-        #db = librosa.power_to_db(np.abs(yf), ref=np.max)
+        
+        # compute periodogram using kaiser windowing function
+        p = Periodogram(block, sampling=sampling_rate, window="kaiser")
+        p.run()
 
         # apply perceptual weighting (make the graph look like how it would sound, I guess)
-        # weighted = librosa.perceptual_weighting(np.abs(yf), xf, ref=np.max, kind="A")
-        # print(weighted)
+        # FIXME doesn't work
+        #weighted = librosa.A_weighting(db)
 
         # we want to sample along yf logarithmically, just like it's graphed
         samples = np.geomspace(FREQ_MIN, FREQ_MAX, NUM_BARS)
-        # print(samples)
 
-        plt.semilogx(xf, np.abs(yf))
+        # convert power spectrum to dB
+        db = 10 * stools.log10(p.psd / max(p.psd))
+        
+        # plot
+        #p.plot(ylim=[-60, 10])
+        plt.semilogx(p.frequencies(), db)
+        ax.set_ylim([-80, 10]) # basically 0 to -80 dB
 
         # draw marks where we would sample
         for sample in samples:
-            plt.axvline(x=sample, color="red")
+            plt.axvline(x=sample, color="grey")
 
         # plt.hist(np.abs(yf), bins=NUM_BARS)
         # plt.plot(xf, np.abs(yf))
